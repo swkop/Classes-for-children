8ec7b66f4b2b116200d502752ba86fb2d319fbd1
Ника <@DESKTOP-KES3G76>
2021-10-14 23:51:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Classes_for_children
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
